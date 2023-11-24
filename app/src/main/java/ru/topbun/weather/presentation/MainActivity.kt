686a31689cd2d60025409bbed4ac2219a851e664
topbun <akimov1712@mail.ru>
2023-11-24 22:59:57 +0300

    Split Project Layers

--- app/src/main/java/ru/topbun/weather/presentation/MainActivity.kt
+++ app/src/main/java/ru/topbun/weather/presentation/MainActivity.kt
@@ -1,8 +1,11 @@
-package ru.topbun.weather
+package ru.topbun.weather.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import ru.topbun.weather.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
